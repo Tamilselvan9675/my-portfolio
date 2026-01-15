2707185543c8f44d0f42073c334b1d97879cadb0
Tamilselvan G <tamilselvan9675@gmail.com>
2026-01-15 21:47:02 +0530

    folder stucture completed

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="h-[60vh] flex items-center justify-center">
+      <h1 className="text-3xl font-bold">404 – Page Not Found</h1>
+    </div>
+  );
+}
